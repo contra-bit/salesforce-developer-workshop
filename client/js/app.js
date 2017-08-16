c48cbf2fde2e8cfb63d9c15e790b6871f5ed5fc4
Liam Hurwitz <liam@contra-bit.com>
2017-08-16 13:27:25 +0200

    no

--- client/js/app.js
+++ client/js/app.js
@@ -1,7 +1,7 @@
 var apiVersion = 'v30.0',
 loginUrl = 'https://login.salesforce.com/',
-redirectURI = 'https://localhost:3000/oauthcallback.html',
-proxyURL = 'https://localhost:3000/proxy/',
+redirectURI = 'https://flybyus.herokuapp.com/:3000/oauthcallback.html',
+proxyURL = 'https://flybyus.herokuapp.com/:3000/proxy/',
 client = new forcetk.Client(clientId, loginUrl, proxyURL);
 
 function login() {
